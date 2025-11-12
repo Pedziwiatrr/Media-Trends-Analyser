8e6923fe1f8f6f47a68fb3304140fbf444feb1e7
Kacper Siemionek <siemionek.kacper22@gmail.com>
2025-11-12 18:26:21 +0100

    update agent config file, add function to generate daily summary prompt

--- services/agent-service/app/core/agent_config.py
+++ services/agent-service/app/core/agent_config.py
@@ -1,7 +1,11 @@
-from pydantic_settings import BaseSettings
+import os
+from pydantic_settings import BaseSettings, SettingsConfigDict
+from dotenv import load_dotenv
+
+load_dotenv()
 
 class AgentSettings(BaseSettings):
-    gemini_api_key: str
+    gemini_api_key: str = os.getenv("GEMINI_API_KEY")
     article_categories: list[str] = [
         "Technology",
         "Politics",
@@ -9,3 +13,20 @@ class AgentSettings(BaseSettings):
         "Sport",
         "Culture"
     ]
+
+    @property
+    def daily_summary_prompt(self):
+        categories_str = ', '.join(self.article_categories)
+
+        return f"""
+            Based on the following articles, generate a summary in JSON format:
+            
+            {{articles}}
+            
+            Return JSON with the following fields:
+            - summary: the most important events of the day
+            - categories: list of article count per category (keys: {categories_str})
+            - references: list of 3-5 URLs to the most important articles of the day
+            
+            Return only JSON, without additional info.
+        """
