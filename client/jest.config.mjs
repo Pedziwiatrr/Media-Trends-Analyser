7790b456a979e221d9c6a705a59500010ee7c5e5
mandrysz <01187204@pw.edu.pl>
2025-11-16 19:15:19 +0100

    Configured jest coverage

--- client/jest.config.mjs
+++ client/jest.config.mjs
@@ -8,6 +8,12 @@ const createJestConfig = nextJest({
 const config = {
   setupFilesAfterEnv: ['<rootDir>/jest.setup.mjs'],
   testEnvironment: 'jest-environment-jsdom',
+  collectCoverageFrom: [
+    'src/components/**/*.{ts,tsx}',
+    '!src/**/*.test.{ts,tsx}',
+    '!src/**/index.ts',
+    '!**/*.d.ts',
+  ],
 };
 
 export default createJestConfig(config);
