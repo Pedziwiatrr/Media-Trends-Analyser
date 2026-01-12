3e9985361a1166bd221191c8d7fd0f5b343f0072
mandrysz <01187204@pw.edu.pl>
2026-01-12 14:12:54 +0100

    Created ScrollToTop component

--- client/src/app/layout.tsx
+++ client/src/app/layout.tsx
@@ -2,6 +2,7 @@ import type { Metadata } from 'next';
 import { Geist, Geist_Mono } from 'next/font/google';
 import { Header } from '@/components/Header';
 import './globals.css';
+import { ScrollToTop } from '@/components/ScrollToTop';
 
 const geistSans = Geist({
   variable: '--font-geist-sans',
@@ -52,6 +53,8 @@ export default function RootLayout({
             </p>
           </footer>
         </div>
+
+        <ScrollToTop />
       </body>
     </html>
   );
