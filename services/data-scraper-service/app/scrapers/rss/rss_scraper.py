c04005b77246f3530e10f994ee0f3ac7c6e0dfa9
Pedziwiatrr <michalozaurus@gmail.com>
2026-01-18 00:21:35 +0100

    revert: removed additional node checks and ruff's imports formatting

--- services/data-scraper-service/app/scrapers/rss/rss_scraper.py
+++ services/data-scraper-service/app/scrapers/rss/rss_scraper.py
@@ -1,20 +1,19 @@
+from app.scrapers.base_scraper import BaseScraper, save_scrapers
+from app.schemas.articles import ArticleCreate
+from app.utils.parser import parse_text
+
 import xml.etree.ElementTree as ET
 from datetime import datetime
-
 import requests
-
-from app.schemas.articles import ArticleCreate
-from app.scrapers.base_scraper import BaseScraper, save_scrapers
-from app.utils.parser import parse_text
 
 
 @save_scrapers
 class RssScraper(BaseScraper):
-    def collect_data(self, category: str | None = None) -> list:
+    def collect_data(self, category: str | None = None) -> list[ArticleCreate]:
         """
         ...
         """
-        data = []
+        data: list[ArticleCreate] = []
         temp_url = self.url % category if category else self.url
 
         try:
@@ -26,16 +25,12 @@ class RssScraper(BaseScraper):
             raise Exception(e)
 
         for item in root.iter("item"):
-            title_node = item.find(".//title")
-            desc_node = item.find(".//description")
-            link_node = item.find(".//link")
+            title = item.find(".//title").text
+            title = parse_text(title)
 
-            if title_node is None or desc_node is None or link_node is None:
-                continue
-
-            title = parse_text(title_node.text)
-            description = parse_text(desc_node.text)
-            url = link_node.text
+            description = item.find(".//description").text
+            description = parse_text(description)
+            url = item.find(".//link").text
 
             if not title or not description or not url:
                 continue
