430db98ed676b184d88fd1d8312e63e34052ee95
mandrysz <01187204@pw.edu.pl>
2026-01-22 12:27:58 +0100

    Decreased revalidation time of daily reports cache to 1 hour

--- client/src/app/(daily)/api.ts
+++ client/src/app/(daily)/api.ts
@@ -1,27 +1,10 @@
 import type { DailyReport } from '@/types/dailyReport';
 import { env } from '@/env';
 
-function getRevalidateTime() {
-  const now = new Date();
-  const target = new Date(now);
-
-  target.setHours(1, 0, 0, 0);
-
-  if (now >= target) {
-    target.setDate(target.getDate() + 1);
-  }
-
-  const secondsUntilUpdate = Math.floor(
-    (target.getTime() - now.getTime()) / 1000
-  );
-
-  return Math.max(60, secondsUntilUpdate);
-}
-
 export async function fetchDailyReports(): Promise<DailyReport[]> {
   const url = `${env.API_URL}/agent/api/v1/daily_summary/recent`;
 
-  const revalidateTime = getRevalidateTime();
+  const revalidateTime = 3600;
 
   try {
     const response = await fetch(url, {
